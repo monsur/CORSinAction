c204da2e6e6a335f8df67a354049fd33177c8cf6
Monsur Hossain <monsur@gmail.com>
2014-03-10 00:18:23 -0500

    First iteration of code outline

---
+++ ch06/cors.js
@@ -0,0 +1,43 @@
+(function() {
+
+  var start = function(res, resp, next) {
+  };
+
+  var allowOrigins = function(options) {
+    return function(res, resp, next) {
+    };
+  };
+
+  var allowCredentials = function(options) {
+    return function(res, resp, next) {
+    };
+  };
+
+  var allowMethods = function(options) {
+    return function(res, resp, next) {
+    };
+  };
+
+  var allowHeaders = function(options) {
+    return function(res, resp, next) {
+    };
+  };
+
+  var maxAge = function(options) {
+    return function(res, resp, next) {
+    };
+  };
+
+  var exposeHeaders = function(options) {
+    return function(res, resp, next) {
+    };
+  };
+
+  var end = function(res, resp, next) {
+  };
+
+  module.exports = {
+    'configure': function(app) {
+    };
+  }
+})();
