370fcc6b292d73d82da8375b4fde6405a802bb49
Monsur Hossain <monsur@gmail.com>
2014-04-13 21:14:12 -0500

    rearranging some code

--- ch06/listing-6.csrf1/app.js
+++ ch06/listing-6.csrf1/app.js
@@ -9,17 +9,18 @@ app.use(bodyParser());
 app.use(cookieParser());
 app.use(session({secret: 'CORSInAction'}));
 app.use(csrf());
-app.use(function(err, req, res, next) {
-  res.send('ERROR parsing CSRF token!');
-});
 app.get('/csrftest', function(req, res) {
   var form = '<html><body><form action="/csrftest" method="post">';
-  form += '<input type="text" name="_csrf" value="' + req.csrfToken() + '" />';
+  form += '<input type="text" name="_csrf" ';
+  form += 'value="' + req.csrfToken() + '" />';
   form += '<input type="submit" value="Submit" />';
   form += '</form></body></html>';
   res.send(form);
 });
 app.post('/csrftest', function(req, res) {
   res.send('Successfully received CSRF token!');
+});
+app.use(function(err, req, res, next) {
+  res.send(403, 'ERROR parsing CSRF token!');
 });
 app.listen(9999);
