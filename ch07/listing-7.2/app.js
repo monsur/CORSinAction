23b00f04a456fd937f5d57a9ea56287f656a5284
Monsur Hossain <monsur@gmail.com>
2014-04-21 23:03:04 -0500

    Adding baseline listing 7.2 (copied from 7.1)

---
+++ ch07/listing-7.2/app.js
@@ -0,0 +1,109 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var serveStatic = require('serve-static');
+
+var POSTS = {
+  '1': {'post': 'This is the first blog post.'},
+  '2': {'post': 'This is the second blog post.'},
+  '3': {'post': 'This is the third blog post.'}
+};
+
+var isPreflight = function(req) {
+  var isHttpOptions = req.method === 'OPTIONS';
+  var hasOriginHeader = req.headers['origin'];
+  var hasRequestMethod = req.headers['access-control-request-method'];
+  return isHttpOptions && hasOriginHeader && hasRequestMethod;
+};
+
+var createWhitelistValidator = function(whitelist) {
+  return function(val) {
+    for (var i = 0; i < whitelist.length; i++) {
+      if (val == whitelist[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+var originWhitelist = [
+  'null',
+  'http://localhost:1111'
+];
+
+var corsOptions = {
+  allowOrigin: createWhitelistValidator(originWhitelist),
+  allowCredentials: true,
+  allowMethods: ['GET', 'DELETE'],
+  allowHeaders: ['Timezone-Offset'],
+  exposeHeaders: ['X-Powered-By']
+};
+
+var handleCors = function(options) {
+  return function(req, res, next) {
+
+    if (options.allowOrigin) {
+      var origin = req.headers['origin'];
+      if (options.allowOrigin(origin)) {
+        res.set('Access-Control-Allow-Origin', origin);
+      }
+      res.set('Vary', 'Origin');
+    } else {
+      res.set('Access-Control-Allow-Origin', '*');
+    }
+
+    if (options.allowCredentials) {
+      res.set('Access-Control-Allow-Credentials', 'true');
+    }
+
+    if (isPreflight(req)) {
+      if (options.allowMethods) {
+        res.set('Access-Control-Allow-Methods',
+            options.allowMethods.join(','));
+      }
+      if (typeof(options.allowHeaders) === 'function') {
+        var headers = options.allowHeaders(req);
+        if (headers) {
+          res.set('Access-Control-Allow-Headers', headers);
+        }
+      } else if (options.allowHeaders) {
+        res.set('Access-Control-Allow-Headers',
+            options.allowHeaders.join(','));
+      }
+      if (options.maxAge) {
+        res.set('Access-Control-Max-Age', options.maxAge);
+      }
+    } else if (options.exposeHeaders) {
+      res.set('Access-Control-Expose-Headers', options.exposeHeaders.join(','));
+    }
+    next();
+  }
+};
+
+var SERVER_PORT = 9999;
+var serverapp = express();
+serverapp.use(cookieParser());
+serverapp.use(serveStatic(__dirname));
+serverapp.use(handleCors(corsOptions));
+serverapp.get('/api/posts', function(req, res) {
+  res.redirect(301, '/api/v2/posts');
+});
+serverapp.get('/api/v2/posts', function(req, res) {
+  res.json(POSTS);
+});
+serverapp.delete('/api/posts/:id', function(req, res) {
+  if (req.cookies['username'] === 'owner') {
+    delete POSTS[req.params.id];
+    res.send(204);
+  } else {
+    res.send(401);
+  }
+});
+serverapp.listen(SERVER_PORT);
+console.log('Started server at http://localhost:' + SERVER_PORT);
+
+var CLIENT_PORT = 1111;
+var clientapp = express();
+clientapp.use(serveStatic(__dirname));
+clientapp.listen(CLIENT_PORT);
+console.log('Started client at http://localhost:' + CLIENT_PORT);
