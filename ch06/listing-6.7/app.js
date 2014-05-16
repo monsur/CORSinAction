5db05cfe5289acea4d1894f788f7028f4a4f34f3
Monsur Hossain <monsur@gmail.com>
2014-05-15 22:28:02 -0500

    Updating listing 6.7 to express4

--- ch06/listing-6.7/app.js
+++ ch06/listing-6.7/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
-var serveStatic = require('serve-static');
 
 var POSTS = {
   '1': {'post': 'This is the first blog post.'},
@@ -27,11 +26,13 @@ var createWhitelistValidator = function(whitelist) {
 };
 
 var originWhitelist = [
+  'null',
   'http://localhost:1111'
 ];
 
 var corsOptions = {
   allowOrigin: createWhitelistValidator(originWhitelist),
+  allowCredentials: true,
   shortCircuit: true
 };
 
@@ -46,17 +47,24 @@ var handleCors = function(options) {
         res.send(403);
         return;
       }
+      res.set('Vary', 'Origin');
     } else {
       res.set('Access-Control-Allow-Origin', '*');
     }
 
-    res.set('Access-Control-Allow-Credentials', 'true');
-    res.set('Access-Control-Expose-Headers', 'X-Powered-By');
+    if (options.allowCredentials) {
+      res.set('Access-Control-Allow-Credentials', 'true');
+    }
+
     if (isPreflight(req)) {
       res.set('Access-Control-Allow-Methods', 'DELETE');
       res.set('Access-Control-Allow-Headers',
               'Timezone-Offset, Sample-Source');
       res.set('Access-Control-Max-Age', '120');
+      res.send(204);
+      return;
+    } else {
+      res.set('Access-Control-Expose-Headers', 'X-Powered-By');
     }
     next();
   }
@@ -65,7 +73,7 @@ var handleCors = function(options) {
 var SERVER_PORT = 9999;
 var serverapp = express();
 serverapp.use(cookieParser());
-serverapp.use(serveStatic(__dirname));
+serverapp.use(express.static(__dirname));
 serverapp.use(handleCors(corsOptions));
 serverapp.get('/api/posts', function(req, res) {
   res.json(POSTS);
@@ -83,6 +91,6 @@ console.log('Started server at http://localhost:' + SERVER_PORT);
 
 var CLIENT_PORT = 1111;
 var clientapp = express();
-clientapp.use(serveStatic(__dirname));
+clientapp.use(express.static(__dirname));
 clientapp.listen(CLIENT_PORT);
 console.log('Started client at http://localhost:' + CLIENT_PORT);
