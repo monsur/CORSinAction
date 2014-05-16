c659f9d7d7138081f8e0c4d0faaf85638c76bb05
Monsur Hossain <monsur@gmail.com>
2014-05-15 22:42:11 -0500

    Updating listing 6.11 to express4

--- ch06/listing-6.11/app.js
+++ ch06/listing-6.11/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 
 var POSTS = {
   '1': {'post': 'This is the first blog post.'},
@@ -32,6 +33,7 @@ var originWhitelist = [
 var corsOptions = {
   allowOrigin: createWhitelistValidator(originWhitelist),
   allowCredentials: true,
+  shortCircuit: true,
   allowMethods: ['GET', 'DELETE'],
   allowHeaders: function(req) {
     var reqHeaders = req.headers['access-control-request-headers'];
@@ -57,6 +59,9 @@ var handleCors = function(options) {
       var origin = req.headers['origin'];
       if (options.allowOrigin(origin)) {
         res.set('Access-Control-Allow-Origin', origin);
+      } else if (options.shortCircuit) {
+        res.send(403);
+        return;
       }
       res.set('Vary', 'Origin');
     } else {
@@ -67,7 +72,6 @@ var handleCors = function(options) {
       res.set('Access-Control-Allow-Credentials', 'true');
     }
 
-    res.set('Access-Control-Expose-Headers', 'X-Powered-By');
     if (isPreflight(req)) {
       if (options.allowMethods) {
         res.set('Access-Control-Allow-Methods',
@@ -83,6 +87,10 @@ var handleCors = function(options) {
             options.allowHeaders.join(','));
       }
       res.set('Access-Control-Max-Age', '120');
+      res.send(204);
+      return;
+    } else {
+      res.set('Access-Control-Expose-Headers', 'X-Powered-By');
     }
     next();
   }
@@ -90,7 +98,7 @@ var handleCors = function(options) {
 
 var SERVER_PORT = 9999;
 var serverapp = express();
-serverapp.use(express.cookieParser());
+serverapp.use(cookieParser());
 serverapp.use(express.static(__dirname));
 serverapp.use(handleCors(corsOptions));
 serverapp.get('/api/posts', function(req, res) {
