95e35ed6879450b93e7853b42f9a1a3a8a941ed7
Monsur Hossain <monsur@gmail.com>
2014-08-06 22:14:52 -0500

    Updating to latest sample

--- appendixC/listing-C.1/app.js
+++ appendixC/listing-C.1/app.js
@@ -1,114 +1,34 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var csrf = require('csurf');
 
-var POSTS = {
-  '1': {'post': 'This is the first blog post.'},
-  '2': {'post': 'This is the second blog post.'},
-  '3': {'post': 'This is the third blog post.'}
-};
+var app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+app.use(session({
+  secret: 'CORSInAction',
+  saveUninitialized: true,
+  resave: true}));
+app.use(csrf());
 
-var isPreflight = function(req) {
-  var isHttpOptions = req.method === 'OPTIONS';
-  var hasOriginHeader = req.headers['origin'];
-  var hasRequestMethod = req.headers['access-control-request-method'];
-  return isHttpOptions && hasOriginHeader && hasRequestMethod;
-};
-
-var createWhitelistValidator = function(whitelist) {
-  return function(val) {
-    for (var i = 0; i < whitelist.length; i++) {
-      if (val === whitelist[i]) {
-        return true;
-      }
-    }
-    return false;
-  }
-};
-
-var originWhitelist = [
-  'null',
-  'http://localhost:1111'
-];
-
-var corsOptions = {
-  allowOrigin: createWhitelistValidator(originWhitelist),
-  allowCredentials: true,
-  shortCircuit: true,
-  allowMethods: ['GET', 'DELETE'],
-  allowHeaders: ['Timezone-Offset', 'Sample-Source'],
-  maxAge: 60,
-  exposeHeaders: ['X-Powered-By']
-};
-
-var handleCors = function(options) {
-  return function(req, res, next) {
-
-    if (options.allowOrigin) {
-      var origin = req.headers['origin'];
-      if (options.allowOrigin(origin)) {
-        res.set('Access-Control-Allow-Origin', origin);
-      } else if (options.shortCircuit) {
-        res.send(403);
-        return;
-      }
-      res.set('Vary', 'Origin');
-    } else {
-      res.set('Access-Control-Allow-Origin', '*');
-    }
-
-    if (options.allowCredentials) {
-      res.set('Access-Control-Allow-Credentials', 'true');
-    }
-
-    if (isPreflight(req)) {
-      if (options.allowMethods) {
-        res.set('Access-Control-Allow-Methods',
-            options.allowMethods.join(','));
-      }
-      if (typeof(options.allowHeaders) === 'function') {
-        var headers = options.allowHeaders(req);
-        if (headers) {
-          res.set('Access-Control-Allow-Headers', headers);
-        }
-      } else if (options.allowHeaders) {
-        res.set('Access-Control-Allow-Headers',
-            options.allowHeaders.join(','));
-      }
-      if (options.maxAge) {
-        res.set('Access-Control-Max-Age', options.maxAge);
-      }
-      res.send(204);
-      return;
-    } else if (options.exposeHeaders) {
-      res.set('Access-Control-Expose-Headers', options.exposeHeaders.join(','));
-    }
-    next();
-  }
-};
-
-var SERVER_PORT = 9999;
-var serverapp = express();
-serverapp.use(cookieParser());
-serverapp.use(express.static(__dirname));
-serverapp.use(handleCors(corsOptions));
-serverapp.get('/api/posts', function(req, res) {
-  res.json(POSTS);
+app.get('/csrftest', function(req, res) {
+  var form = '<html><body><form action="/csrftest" method="post">\r\n';
+  form += '<input type="text" name="_csrf" \r\n';
+  form += '\tvalue="' + req.csrfToken() + '" />\r\n';
+  form += '<input type="submit" value="Submit" />\r\n';
+  form += '</form></body></html>';
+  res.send(form);
 });
-serverapp.delete('/api/posts/:id', function(req, res) {
-  if (req.cookies['username'] === 'owner') {
-    delete POSTS[req.params.id];
-    res.send(204);
-  } else {
-    res.send(403);
-  }
-});
-serverapp.listen(SERVER_PORT, function() {
-  console.log('Started server at http://127.0.0.1:' + SERVER_PORT);
+app.post('/csrftest', function(req, res) {
+  res.send('Successfully received CSRF token!');
 });
 
-var CLIENT_PORT = 1111;
-var clientapp = express();
-clientapp.use(express.static(__dirname));
-clientapp.listen(CLIENT_PORT, function() {
-  console.log('Started client at http://localhost:' + CLIENT_PORT);
+app.use(function(err, req, res, next) {
+  res.send(403, 'ERROR parsing CSRF token!');
 });
+
+app.listen(2468);
