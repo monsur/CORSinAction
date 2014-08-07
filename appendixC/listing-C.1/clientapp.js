82d725e73eb49dfd728bbc2da515854533367c92
Monsur Hossain <monsur@gmail.com>
2014-08-06 22:11:16 -0500

    Revert "Adding outline for newpost page"
    This reverts commit 19e5595f519ecb08bb4f4a76adfb219879aa45ef.

--- appendixC/listing-C.1/clientapp.js
+++ appendixC/listing-C.1/clientapp.js
@@ -5,8 +5,8 @@ var clientapp = express();
 clientapp.set('views', __dirname);
 clientapp.set('view engine', 'jade');
 clientapp.use(express.static(__dirname));
-clientapp.get('/newpost', function(req, res){
-  res.render('newpost', { });
+clientapp.get('/client.html', function(req, res){
+  res.render('client', { });
 });
 clientapp.listen(CLIENT_PORT, function() {
   console.log('Started client at http://localhost:' + CLIENT_PORT);
